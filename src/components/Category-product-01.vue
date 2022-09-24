f97ec147edbb1409c164c47b3ee98b8289178e61
bams <bbg2021@gmail.com>
2022-09-24 15:31:57 +0800

    add section

---
+++ src/components/Category-product-01.vue
@@ -0,0 +1,17 @@
+<template>
+    <section id="benefit-01" class=" max-w-6xl mx-auto  py-8 md:my-16 lg:my-24 text-center ">
+        <div class="bg-brandPrimary text-white py-1 px-5 rounded-full inline-block text-sm mb-3">Tour & Travel</div>
+            <h2 class="text-3xl font-semibold text-gray-800 mb-6 md:mb-5">Pilih mobil yang sesuai dengan kebutuhanmu</h2>
+            <p>
+                Dengan banyaknya pilihan mobil yang bisa kamu rental,  kamu bisa mencoba satu per satu yang kamu mau, berkendara di Bali tentu saja akan memberikan pengalaman liburan yang mengasyikan, tapi ingat  jaga selalu keselamatan.  
+            </p>
+         <ul class="grid grid-cols-1 sm:grid-cols-3 mx-auto gap-y-8 md:gap-y-0">
+            <li class="rounded-lg bg-white p-4"> 
+                <img class="mx-auto h-auto w-80 rounded-lg " src="../../public/images/expander.png" alt=""> 
+            </li>
+         </ul>
+      
+       
+    </section>
+   
+</template>
--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,14 +1,20 @@
 <template>
    <Hero01/>
    <AirportTransfer/>
+   <WorkWith01/>
+   <Benefit01/>
+   <CategoryProduct01/>
 </template>
 
 <script>
 import Hero01 from '../components/Hero-01.vue';
 import AirportTransfer from '../components/Airport-Transfer.vue';
+import WorkWith01 from '../components/Work-With-01.vue';
+import Benefit01 from '../components/Benefit-01.vue';
+import CategoryProduct01 from '../components/Category-product-01.vue';
 export default {
     name: "PageHome",
-    components: { Hero01, AirportTransfer }
+    components: { Hero01, AirportTransfer, WorkWith01, Benefit01, CategoryProduct01 }
 }
 </script>
 
