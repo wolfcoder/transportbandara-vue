33eb95595337e58d1b5c8ed32f4afa440baf8c00
iwankrisnawan <iwankrisnawan03@gmail.com>
2022-10-04 06:53:20 +0800

    convert page category car

---
+++ src/components/RentalMobilCategory/CarCategoryProduct-03.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="max-w-6xl mx-auto py-8 md:my-16 lg:my-24 text-center">
+    <div
+      class="
+        bg-brandTertiary
+        text-white
+        py-1
+        px-5
+        rounded-full
+        inline-block
+        text-sm
+        mb-3
+      "
+    >
+      Mini Bus
+    </div>
+    <h2 class="text-4xl font-semibold text-gray-800 mb-6 md:mb-5">
+      Pilih mobil yang sesuai dengan kebutuhanmu
+    </h2>
+    <p class="mb-12 max-w-4xl mx-auto text-gray-400 px-4 md:px-0">
+      Dengan banyaknya pilihan mobil yang bisa kamu rental, kamu bisa mencoba
+      satu per satu yang kamu mau, berkendara di Bali tentu saja akan memberikan
+      pengalaman liburan yang mengasyikan, tapi ingat jaga selalu keselamatan.
+    </p>
+    <ul class="grid grid-cols-1 sm:grid-cols-3 mx-auto gap-12 px-4 md:px-0">
+      <CarCategoryProduct03Item v-for="index in 3" :key="index" />
+    </ul>
+  </section>
+</template>
+
+<script>
+import CarCategoryProduct03Item from "./CarCategoryProduct-03-Item.vue";
+export default {
+  name: "CarPage",
+  components: { CarCategoryProduct03Item },
+};
+</script>
---
+++ src/components/RentalMobilCategory/Hero-01.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="bg-[#1B96BC] my-8 md:my-16 lg:my-24">
+    <div
+      class="
+        mx-auto
+        flex
+        justify-between
+        flex-col
+        lg:flex-row
+      "
+    >
+      <div class="hero-text px-4 w-full w-full lg:w-8/12 flex flex-col gap-6 py-10 lg:pl-32">
+        <p class="text-white text-2xl">
+          Pilih mobil yang sesuai dengan kebutuhanmu
+        </p>
+        <p class="text-white">
+          Dengan banyaknya pilihan mobil yang bisa kamu rental, kamu bisa
+          mencoba satu per satu yang kamu mau, berkendara di Bali tentu saja
+          akan memberikan pengalaman liburan yang mengasyikan, tapi ingat jaga
+          selalu keselamatan.
+        </p>
+      </div>
+      <div class="lg:bg-[#AFD7E3] lg:rounded-l-full p-5">
+        <div class="lg:-ml-24">
+          <img
+          src="../../../public/images/multi-car.png"
+          alt="liburan di bali"
+          class=""
+        />
+        </div>
+      </div>
+    </div>
+  </section>
+  
+</template>
+  
